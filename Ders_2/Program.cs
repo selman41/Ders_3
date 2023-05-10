0d8a604d8447559e1b0eed9fd605fcac67e43255
selma <selma@SELMAN_YILDIRIM>
2023-05-10 20:19:59 +0300

    Proje dosyası ekle.

---
+++ Ders_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+// döngüler
+/*
+for (int i = 0; i <10; i++)                         // döngüde i değerini sıfır dedik ardından i 'nin 10 'dan küçük olduğunu öğrendik ve i 'nin birer birer artığını gördük
+{
+    Console.WriteLine("i'nin değerleri: "+i);
+}
+*/      
+int i = 0;                                          // i değerini sıfır olarak atadık
+while (i<10)                                        // bir while döngüsü oluşturduk
+{
+    Console.WriteLine("i'nin değerleri: " + i);
+    i++;                                             // döngünün her seferde artmasını sağladık
+}
